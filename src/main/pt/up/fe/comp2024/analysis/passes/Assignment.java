a687e44797c780405404159fc62be067a8cc9a86
Máximo Pereira <up202108887@up.pt>
2024-05-23 22:57:23 +0100

    added array element initialization

--- src/main/pt/up/fe/comp2024/analysis/passes/Assignment.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/Assignment.java
@@ -20,6 +20,7 @@ public class Assignment extends AnalysisVisitor {
     protected void buildVisitor() {
         addVisit(Kind.METHOD_DECL,this::visitMethodDecl);
         addVisit(Kind.ASSIGN_STMT,this::visitAssignStmt);
+        addVisit(Kind.ASSIGN_STMT_ARRAY,this::visitAssignStmtArray);
     }
 
     private Void visitMethodDecl(JmmNode method, SymbolTable table) {
@@ -79,6 +80,63 @@ public class Assignment extends AnalysisVisitor {
                 Stage.SEMANTIC,
                 NodeUtils.getLine(assignStmt),
                 NodeUtils.getColumn(assignStmt),
+                message,
+                null)
+        );
+        return null;
+    }
+
+    private Void visitAssignStmtArray(JmmNode assignStmtArray, SymbolTable table) {
+        SpecsCheck.checkNotNull(currentMethod, () -> "Expected current method to be set");
+
+        String varName = assignStmtArray.get("name");
+
+        boolean declared = false;
+        // Var is a local variable
+        if (table.getLocalVariables(currentMethod).stream()
+                .anyMatch(varDecl -> varDecl.getName().equals(varName))) {
+            declared = true;
+        }
+
+        // Var is a parameter
+        if (!declared && table.getParameters(currentMethod).stream()
+                .anyMatch(param -> param.getName().equals(varName))) {
+            declared = true;
+        }
+
+        // Var is a field and method is not static
+        if (!declared && table.getFields().stream()
+                .anyMatch(param -> param.getName().equals(varName)) &&
+                !Boolean.parseBoolean(currentMethodNode.get("isStatic"))) {
+            declared = true;
+        }
+
+        // if variable is not declared
+        if (!declared) {
+            // Create error report
+            var message = String.format("Variable '%s' does not exist.", varName);
+            addReport(Report.newError(
+                    Stage.SEMANTIC,
+                    NodeUtils.getLine(assignStmtArray),
+                    NodeUtils.getColumn(assignStmtArray),
+                    message,
+                    null)
+            );
+
+            return null;
+        }
+
+        Type left = TypeUtils.getExprType(assignStmtArray.getChild(0), table);
+        Type right = TypeUtils.getExprType(assignStmtArray.getChild(1), table);
+
+        if (left != null && right != null
+                && TypeUtils.areTypesAssignable(left, right, table)) return null;
+
+        var message = String.format("Variable '%s' is assigned to invalid value.", varName);
+        addReport(Report.newError(
+                Stage.SEMANTIC,
+                NodeUtils.getLine(assignStmtArray),
+                NodeUtils.getColumn(assignStmtArray),
                 message,
                 null)
         );
